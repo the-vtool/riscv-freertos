d6866f7c7d8960cb1744d09d895118acb4af296c
Zarko Milojicic <zarkom@thevtool.com>
2022-12-15 18:14:08 +0100

    fixed arch link error

--- riscv64-unknown-elf-gcc.cmake
+++ riscv64-unknown-elf-gcc.cmake
@@ -1,14 +1,14 @@
 set(CMAKE_SYSTEM_NAME               Generic)
-set(CMAKE_SYSTEM_PROCESSOR          riscv)
+set(CMAKE_SYSTEM_PROCESSOR          rv32imac)
 
 # Some default GCC settings
 # arm-none-eabi- must be part of path environment
 set(TOOLCHAIN_PREFIX                riscv64-unknown-elf-)
-set(FLAGS                           "-march=rv32imac -mabi=ilp32 -fdata-sections -ffunction-sections -Wl,--gc-sections ")
-set(CPP_FLAGS                       "-fno-rtti -fno-exceptions -fno-threadsafe-statics")
-set(TOOLCHAIN_PATH                  "/home/zarko/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-linux-ubuntu14/bin")
+set(FLAGS                           "-march=rv32imac -mabi=ilp32 -mcmodel=medlow -fdata-sections -ffunction-sections -Wl,--gc-sections ")
+set(CPP_FLAGS                       "-march=rv32imac -mabi=ilp32 -mcmodel=medlow -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(TOOLCHAIN_PATH                  "/home/zarko/.platformio/packages/toolchain-riscv/bin")
 
-# "/home/zarko/.platformio/packages/toolchain-riscv/bin"
+# "/home/zarko/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-linux-ubuntu14/bin /home/zarko/.platformio/packages/toolchain-riscv/bin"
 
 # Define compiler settings
 set(CMAKE_C_COMPILER                ${TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}gcc)
@@ -33,8 +33,8 @@ set(linker_script_SRC               ${CMAKE_CURRENT_SOURCE_DIR}/link.lds)
 # added -specs=picolibc.specs because stdint.h wasn't found
 # for picolibc.specs
 # sudo apt install picolibc-riscv64-unknown-elf
-# riscv64-unknown-elf-gcc -specs=picolibc.specs -march=rv32i -mabi=ilp32 -c test.c
-set(CPU_PARAMETERS "")
+# riscv64-unknown-elf-gcc -specs=picolibc.specs -march=rv32imac -mabi=ilp32 -c test.c -m elf32lriscv   -march=rv32imac -mabi=ilp32 -mcmodel=medlow 
+set(CPU_PARAMETERS "-march=rv32imac -mabi=ilp32 -mcmodel=medlow")
 
 if( NOT C_FLAGS_INITIALIZED )
     # only do this on the first pass through to avoid overwriting user added options.
@@ -42,13 +42,13 @@ if( NOT C_FLAGS_INITIALIZED )
 
     # Overwrite CMake's defaults... 
 
-    string(APPEND CMAKE_C_FLAGS         "${CPU_PARAMETERS} -Wall -Wextra -Wpedantic -Wno-unused-parameter -Og -g3 -ggdb" )
+    string(APPEND CMAKE_C_FLAGS         "${CPU_PARAMETERS} -Wall -Wextra -Wpedantic -Wno-unused-parameter -Og -g3 -ggdb")
     string(APPEND CMAKE_CXX_FLAGS       "-fno-rtti -fno-exceptions -fno-threadsafe-statics")
     # set( CMAKE_C_FLAGS_DEBUG          "-g -DDEBUG")
     # set( CMAKE_C_FLAGS_RELEASE        "-O3 -DNDEBUG" )
     # set( CMAKE_C_FLAGS_MINSIZEREL     "${CMAKE_C_FLAGS_RELEASE}" )
     # set( CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -g -gdwarf-3" )
-    string(APPEND CMAKE_EXE_LINKER_FLAGS       "-T${linker_script_SRC} ${CPU_PARAMETERS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map --specs=nosys.specs -u _printf_float -Wl,--start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group -Wl,--print-memory-usage")
+    string(APPEND CMAKE_EXE_LINKER_FLAGS       "-T${linker_script_SRC} ${CPU_PARAMETERS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map --specs=nosys.specs -u _printf_float -Wl ,--start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group -Wl,--print-memory-usage")
 
 endif()
 
@@ -72,10 +72,9 @@ set(symbols_SYMB
     "D_TIMER_DURATION_SETUP_ADDRESS=0x80001030"
     "D_TIMER_ACTIVATION_ADDRESS=0x80001034"
     "D_NMI_VEC_ADDRESSS=0x8000100C"
-    "__riscv_xlen=32"
     "portasmHANDLE_INTERRUPT=pspExtIntHandler"
     
-    
+    #"__riscv_xlen=32" #even if I set -march=rv32imac -mabi=ilp32 this macro is not given by compiler
     # "USE_HAL_DRIVER"
     # "STM32H743xx"
     # "USE_FULL_LL_DRIVER"
@@ -91,10 +90,9 @@ set(symbols_SYMB
 #
 add_compile_definitions(${symbols_SYMB})
 
-
 set(CMAKE_C_FLAGS            "${CPU_PARAMETERS} -Wall -Wextra -Wpedantic -Wno-unused-parameter -Og -g3 -ggdb")
 set(CMAKE_CXX_FLAGS          "-fno-rtti -fno-exceptions -fno-threadsafe-statics")
-set(CMAKE_EXE_LINKER_FLAGS   "-T${linker_script_SRC} ${CPU_PARAMETERS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map --specs=nosys.specs --enable-multilib -u _printf_float -Wl,--start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group -Wl,--print-memory-usage")
+set(CMAKE_EXE_LINKER_FLAGS   "-T${linker_script_SRC} ${CPU_PARAMETERS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map --specs=nosys.specs -u _printf_float -Wl,--start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group -Wl,--print-memory-usage")
 
 # Save the current compiler flags to the cache every time cmake configures the project.
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "compiler flags" FORCE)
